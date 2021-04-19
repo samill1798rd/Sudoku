69cbd891c531cf121f876135a2d5e34d986ff0d4
Emilio Rosa <me@emiliort.com>
2021-04-19 10:36:46 -0400

    funcion main declarada

--- sudoku.py
+++ sudoku.py
@@ -4,3 +4,13 @@ def sudokuResolve(tablero):
     tableroResuelto = '382746519467519832915283647254897361731625984896431725543962178629178453178354296 BTS'
 
     return tableroResuelto
+
+
+def main():
+    tableroInicial:str = input()
+
+    tableroFinal:str = sudokuResolve(tableroInicial)
+
+
+if __name__ == '__main__':
+    main()
